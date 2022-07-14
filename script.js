466e5ccaa92a1c30f44c1ba14d72bd3d52e232ae
timsilber <tim.silber@me.com>
2022-07-13 20:55:55 -0400

    Add letter to board onclick

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 //board template
 
-const Player = (name, marker) => {
+const Player = (name, number) => {
 
-    let plays = [] //index of plays
+    let plays = []
 
-    const addToPlays = (square) => {
-        this.play
+    markerChoices = ['<div class="marker X">X</div>', '<div class="marker O">O</div>']
+   
+    const addPlay = (square) => {
+        const squareIndex= square.target.dataset.square - 1
+        plays[squareIndex] = true
+        console.log(plays)
     }
 
-    return {name};
+    let marker = markerChoices[number-1]
+
+    return {name, marker, addPlay};
 }
 
 const Gameboard = (() => {
 
-    //start game
+    const plays = [null, null, null, null, null, null, null, null, null] //index of plays
+
+    const board = [...document.querySelector('.container').children]
+
+    const clickListener = (() => {
+        board.forEach(square => {
+            square.addEventListener('click', (square) => {
+                makeMark(square, player1)
+                player1.addPlay(square);
+             })
+        })
+    })
+
+    const makeMark = (square, player) => {
+        const squareToMark=square.target
+        squareToMark.innerHTML= player.marker
+    }
 
     //input player name
 
@@ -21,21 +43,10 @@ const Gameboard = (() => {
     
     //start new game 
 
-    return {}
+    return {clickListener, makeMark} 
 })();
 
-const displayController = ((board) => {
-
-    const clickListener = () => {
-        const board = [...document.querySelector('.container').children]
-        board.forEach(square => {
-            square.addEventListener('click', (e) => {
-                const s = e.target.dataset.square - 1
-                console.log(s)
-                return {s}
-             })
-        })
-    }
+const displayController = (() => {
 
     const checkPlay = () => {
         //check if square is not played or remove event listener from square after play
@@ -48,8 +59,9 @@ const displayController = ((board) => {
     const checkWin = () => {
       //check if three in a row
     }
-
-    return {clickListener} 
 })();
 
-console.log(displayController.clickListener())
+const player1 = Player("tim", 1)
+const player2 = Player("loser", 2)
+
+console.log(Gameboard.clickListener())
