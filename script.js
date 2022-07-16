2f114ee2c18bbe2c76da17437044ce05622aff00
timsilber <tim.silber@me.com>
2022-07-16 11:02:16 -0400

    Complete game logic. TODO AI

--- script.js
+++ script.js
@@ -2,48 +2,60 @@
 
 const Player = (name, number) => {
 
-    let plays = []
+
+    let moveList = []
 
     markerChoices = ['<div class="marker X">X</div>', '<div class="marker O">O</div>']
-   
-    const addPlay = (square) => {
-        const squareIndex= square.target.dataset.square - 1
-        plays[squareIndex] = true
-        console.log(plays)
-    }
 
     let marker = markerChoices[number-1]
 
-    return {name, marker, addPlay};
+    const checkWin = (moveList) => {
+       Gameboard.winningCombos.forEach(combo => {
+        if (moveList.length >= 3 && isSubset(moveList, combo)){
+            Gameboard.winner=name;
+            console.log(name)
+            Gameboard.showFireworks();
+        }
+       })
+    }
+
+    const isSubset = (array1, array2) => array2.every(element => array1.includes(element));
+
+    return {name, marker, moveList, checkWin};
 }
 
 const Gameboard = (() => {
 
-    const plays = [null, null, null, null, null, null, null, null, null] //index of plays
+    const board = [null, null, null, null, null, null, null, null, null] //index of plays
 
-    const board = [...document.querySelector('.container').children]
+    const winningCombos = [
+        [0,1,2],
+        [0,3,6],
+        [3,4,5],
+        [6,7,8],
+        [1,4,7],
+        [2,4,6],
+        [2,5,8],
+        [0,4,8]
+    ]
 
-    const clickListener = ((player) => {
-        board.forEach(square => {
-            square.addEventListener('click', (square) => {
-                makeMark(square, player)
-                player.addPlay(square);
-             })
-        })
-    })
+    const winner = null
 
     const makeMark = (square, player) => {
         const squareToMark=square.target
-        squareToMark.innerHTML= player.marker
+        squareToMark.innerHTML= player.marker;
+        return true
     }
 
-    //input player name
+    const showFireworks = () => {
+    const fireworks = document.createElement('div');
+    fireworks.classList.add('pyro');
+    fireworks.innerHTML = `<div class="before"></div><div class="after"></div>`
+    const body = document.querySelector('body')
+    body.insertBefore(fireworks, body.firstChild);
+    }
 
-    //assign marker
-    
-    //start new game 
-
-    return {clickListener, makeMark} 
+    return {makeMark, winner, board, winningCombos, showFireworks} 
 })();
 
 const displayController = (() => {
@@ -52,23 +64,42 @@ const displayController = (() => {
     const player2 = Player("loser", 2)
 
    const startGame = () => {
-        //input player names
-   }
+    player1.turn = true
+    player2.turn = false
+    }
 
-   const playRound = (player) => {
-        Gameboard.clickListener(player);
-        // player = player2
-   }
+   const playTurn = ()=>{
+        const squares = [...document.querySelectorAll('.square')]
+
+        squares.forEach( square => {
+            square.addEventListener('click', e => {
+                if (player1.turn == true && Gameboard.winner == null
+                    && e.target.textContent == '') {
+                    player1.moveList.push(e.target.dataset.square-1);
+                    square.innerHTML = player1.marker;
+                    square.style.color = '#EE6C4D';
+                    player1.turn = false;
+                    player2.turn = true;
+                    player1.checkWin(player1.moveList);
+                } else if (player2.turn == true && Gameboard.winner == null
+                    && e.target.textContent == '') {
+                    player2.moveList.push(e.target.dataset.square-1);
+                    Gameboard.board[e.target.id] = player2.marker;
+                    square.innerHTML = player2.marker;
+                    square.style.color = '#98C1D9';
+                    player1.turn = true;
+                    player2.turn = false;
+                    player2.checkWin(player2.moveList)
+
+                    } else {
+                    return;
+                };
+            });
+        });
+    };
+
 
     const gameOver = false
-
-    //const playGame = while (gameOver==false){
-        //player1 turn
-        //checkwin
-        //player2turn
-        //checkwin
-        //on win return gameOver==true
-    // }
     
     const checkPlay = () => {
         //check if square is not played or remove event listener from square after play
@@ -78,18 +109,12 @@ const displayController = (() => {
         //add square to board
     }
 
-    const checkWin = () => {
-      //check if three in a row
-    }
-
-    return {startGame, gameOver, playRound}
+    return {player1, player2, startGame, gameOver, playTurn}
 
 })(Gameboard);
 
-const player1 = Player("tim", 1)
-const player2 = Player("loser", 2)
 
-displayController.playRound(player1)
-// setTimeout(10000)
-// displayController.playRound(player2)
+// displayController.playRound();
 
+displayController.startGame();
+displayController.playTurn();
